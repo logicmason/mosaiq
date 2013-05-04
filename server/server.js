cf1ce0872af774644743f9433eed5a462fafc989
Tim Sze <timmysze@gmail.com>
2013-05-04 16:13:18 -0700

    wrote blank server

--- server/server.js
+++ server/server.js
@@ -1,3 +1,28 @@
-Meteor.startup(function () {
- // code to run on server at startup
-});
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , app = express()
+  , server = require('http').createServer(app);
+
+server.listen(3000);
+console.log('Express server listening on port 3000');
+
+// Config
+
+app.use(express.favicon());
+// app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(cookieParser);
+app.use(express.session({ secret: secret, store: store }));
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+// Routes
+
+app.get('/', );
