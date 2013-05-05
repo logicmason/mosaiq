376d2c59700443ac018a45fa150f6accf6f26d9a
Tim Sze <timmysze@gmail.com>
2013-05-04 19:27:06 -0700

    Averaging Hues working!!

--- client/mosaiq.js
+++ client/mosaiq.js
@@ -12,7 +12,95 @@ Template.greeting.events({
 		ctx.drawImage(mypic, 0, 0);
 		data = ctx.getImageData(0,0,50,50).data;
 		console.log(data);
+    console.log(calcAverageHue(data));
   }
 });
 
+var getImageArray = function (src) {
+  var canvas = $('<canvas>');
+  var ctx = canvas[0].getContext('2d');
+  var pic = new Image();
+  pic.src = src;
+  ctx.drawImage(pic, 0, 0);
+  return ctx.getImageData(0,0,50,50).data;
+};
 
+
+var hueToXY = function (value) {
+  value = value * Math.PI / 180;
+  var x = Math.cos(value);
+  var y = Math.sin(value);
+  return [x, y];
+};
+
+var convertToPolar = function () {
+
+};
+
+var calcAverageHue = function (array) {
+  var hueArray = [];
+  var hsvData;
+  // var numPixels = array.length / 4;
+  var iterations = 0;
+
+  for (var i = 0, l = array.length; i < l; i += 4) {
+    iterations++;
+    hsvData = rgb2hsv(array[i], array[i+1], array[i+2]);
+    // output.push(hsvData);
+    hueArray.push(hsvData.h);
+    // console.log(hsvData);
+  }
+
+  var polarArray = hueArray.map(hueToXY);
+  var sumVector = polarArray.reduce(function (a, b) {
+    return [a[0] + b[0], a[1] + b[1]];
+  });
+
+  var radians = Math.atan(sumVector[1] / sumVector[0]);
+  if (radians < 0) {
+    radians += 2 * Math.PI;
+  }
+  var averageHue = radians / Math.PI * 180;
+
+  return averageHue;
+};
+
+var rgb2hsv = function (r,g,b) {
+  var rr, gg, bb,
+      r = arguments[0] / 255,
+      g = arguments[1] / 255,
+      b = arguments[2] / 255,
+      h, s,
+      v = Math.max(r, g, b),
+      diff = v - Math.min(r, g, b),
+      diffc = function(c){
+          return (v - c) / 6 / diff + 1 / 2;
+      };
+
+  if (diff === 0) {
+      h = s = 0;
+  } else {
+      s = diff / v;
+      rr = diffc(r);
+      gg = diffc(g);
+      bb = diffc(b);
+
+      if (r === v) {
+          h = bb - gg;
+      }else if (g === v) {
+          h = (1 / 3) + rr - bb;
+      }else if (b === v) {
+          h = (2 / 3) + gg - rr;
+      }
+      if (h < 0) {
+          h += 1;
+      }else if (h > 1) {
+          h -= 1;
+      }
+  }
+  return {
+      h: Math.round(h * 360),
+      s: Math.round(s * 100),
+      v: Math.round(v * 100)
+  };
+};
